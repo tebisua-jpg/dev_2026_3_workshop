4998e315fb30d797404fb3d894b722cc2486ee1a
tebisua-jpg <tebisua@gmail.com>
2026-08-31 23:30:41 -0500

    games 3.2

--- src/games/games.py
+++ src/games/games.py
@@ -60,24 +60,26 @@ class Games:
              ["O", "O", " "],
              [" ", " ", " "]] -> "X"
         """
-        for i in range(3):
-         if tablero[i][0] != " " and tablero[i][0] == tablero[i][1] == tablero[i][2]:
-                return tablero[i][0]
+        lineas = []
 
-         if tablero[0][i] != " " and tablero[0][i] == tablero[1][i] == tablero[2][i]:
-                return tablero[0][i]
+        lineas.extend(tablero)
 
-        if tablero[0][0] != " " and tablero[0][0] == tablero[1][1] == tablero[2][2]:
-            return tablero[0][0]
+        for columna in range(3):
+          lineas.append([tablero[0][columna], tablero[1][columna], tablero[2][columna]])
 
-        if tablero[0][2] != " " and tablero[0][2] == tablero[1][1] == tablero[2][0]:
-            return tablero[0][2]
+        lineas.append([tablero[0][0], tablero[1][1], tablero[2][2]])
+        lineas.append([tablero[0][2], tablero[1][1], tablero[2][0]])
+
+        for linea in lineas:
+          if linea[0] != " " and linea[0] == linea[1] == linea[2]:
+           return linea[0]
 
         for fila in tablero:
-            if " " in fila:
-                return "continua"
+         if " " in fila:
+          return "continua"
 
         return "empate"
+        
 
         pass
     
